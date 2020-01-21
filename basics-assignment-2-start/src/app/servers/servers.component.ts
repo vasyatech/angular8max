531debedbbe4576f6e4d1f68e017953c5113f095
Vasya <vtarasov@componentcontrol.com>
2020-01-20 17:37:30 -0800

    Assignment 2: Practicing Databinding

---
+++ basics-assignment-2-start/src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  userName = '';
+  isEmptyUser = true;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSaveClick(){
+    this.userName = '';
+  }
+}
