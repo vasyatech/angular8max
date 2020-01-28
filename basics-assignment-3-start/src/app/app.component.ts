1595bdc9ba1fb17f358acf47593902f911a3fef1
Vasya <vtarasov@componentcontrol.com>
2020-01-28 12:10:31 -0800

    Assignment 3: Practicing Directives. Final commit

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  logItems = [];
+  showContent = false;
+  buttonCaption = 'Display Details';
+  logInfo = '';
+  
+  onShowHideContent(){
+    
+    this.showContent = !this.showContent;
+    if (!this.showContent){
+      this.buttonCaption = 'Display Details';
+      this.logInfo = 'Content was hidden on: ' + new Date().toLocaleString();
+      this.logItems.push(this.logInfo);
+    }
+    else{
+      this.buttonCaption = 'Hide Details';
+      this.logInfo = 'Content was shown on: ' + new Date().toLocaleString();
+      this.logItems.push(this.logInfo);
+    }
+    
+  }
 }
